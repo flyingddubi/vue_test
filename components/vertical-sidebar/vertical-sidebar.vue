3e74df823f4c713c4236949c0d7604b61f560d27
2130056 <sangminem@gmail.com>
2024-11-12 11:06:26 +0900

    자식 부모간 데이터 전달 샘플

---
+++ components/vertical-sidebar/vertical-sidebar.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const items = ref([
+  { message: "출원진행", link: "/tips_11/tips_11_11" },
+  { message: "종료관리", link: "/tips_11/tips_11_22" },
+]);
+</script>
+
+<template>
+  <div class="sidebar">
+    <v-list class="pa-6" density="compact">
+      <ul>
+        <li v-for="item in items">
+          <nuxt-link to="/">{{ item.message }}</nuxt-link>
+        </li>
+      </ul>
+    </v-list>
+  </div>
+  <div>
+    <h2>sidebar data: {{ verticalSidebarData }}</h2>
+  </div>
+</template>
+
+<style>
+.item {
+  cursor: pointer;
+  line-height: 1.5;
+}
+.bold {
+  font-weight: bold;
+}
+
+/* .sidebar {
+  
+  position: fixed;
+  top: 110px;
+  left: 0;
+  height: 100%;
+  width: 200px;
+
+  margin:0 !important;
+  z-index: 1000;
+  display: flex;
+
+  border: 2px solid red;
+  background-color: white;
+} */
+</style>
+
+<script lang="ts">
+export default {
+  props: {
+    verticalSidebarData: "sidebar",
+  },
+};
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import TipsHeader from "~/\bcomponents/tips-header/tips-header.vue";
+import VerticalSidebar from "~/\bcomponents/vertical-sidebar/vertical-sidebar.vue";
+</script>
+
+<template>
+  <div style="border: 2px solid red">
+    <h1>헤더입니다.</h1>
+    <TipsHeader @layoutFunc="layoutFunc" />
+  </div>
+  <div>
+    <h1>레이아웃입니다..</h1>
+    <h2>layout data: {{ layoutData }}</h2>
+  </div>
+  <div>
+    <h1>사이드바입니다.</h1>
+    <VerticalSidebar :verticalSidebarData="layoutData" />
+  </div>
+</template>
+
+<style scoped></style>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      layoutData: "layout",
+    };
+  },
+  methods: {
+    layoutFunc(data: string) {
+      this.layoutData = data;
+    },
+  },
+};
+</script>
